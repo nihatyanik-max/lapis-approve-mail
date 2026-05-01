10bd80b333617c0ad459894ee3c87f2448010ffe
nihatyanik-max <nihatyanik@gmail.com>
2026-05-02 00:56:33 +0300

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.0"
 import nodemailer from "npm:nodemailer@6.9.1"
 
@@ -42,7 +41,7 @@ function sendNotification(d, action, comment) {
   })
 }
 
-serve(async (req) => {
+Deno.serve(async (req) => {
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: { "Access-Control-Allow-Origin": "*" } })
   }
